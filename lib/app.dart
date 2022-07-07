e110bf13594ded5baf3f11a021be5befa99332b6
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-07 17:07:09 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
+import 'text_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); //1.constructor
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) { //2.build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: TextPage(),
     );
   }
 }
